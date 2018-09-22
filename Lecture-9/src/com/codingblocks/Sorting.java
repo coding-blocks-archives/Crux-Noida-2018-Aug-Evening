2effa7d02c3baa1c6b192fadf341d7f368eb6faa
KeenWarrior <anujgargcse@gmail.com>
2018-09-22 15:56:14 +0530

    Merge sort

--- Lecture-9/src/com/codingblocks/Sorting.java
+++ Lecture-9/src/com/codingblocks/Sorting.java
@@ -6,9 +6,9 @@ public class Sorting {
     public static void main(String[] args) {
         int[] nums = {3, 2, 1, 9, 4};
 
-        int[] sorted = mergeSort(nums);
+        mergeSortInPlace(nums, 0, nums.length);
 
-        System.out.println(Arrays.toString(sorted));
+        System.out.println(Arrays.toString(nums));
     }
 
     public static int[] mergeSort(int[] nums){
@@ -22,6 +22,47 @@ public class Sorting {
         int[] second = mergeSort(Arrays.copyOfRange(nums, mid, nums.length));
 
         return merge(first, second);
+    }
+
+    public static void mergeSortInPlace(int[] nums, int start, int end){
+        if (end - start == 1){
+            return;
+        }
+
+        int mid = (start + end)/2;
+
+        mergeSortInPlace(nums, start, mid);
+        mergeSortInPlace(nums, mid, end);
+
+        mergeInPlace(nums, start, mid, end);
+    }
+
+    private static void mergeInPlace(int[] nums, int start, int mid, int end) {
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        int[] mix = new int[end - start];
+
+        while (i < mid && j < end){
+            if (nums[i] < nums[j]){
+                mix[k++] = nums[i++];
+            } else {
+                mix[k++] = nums[j++];
+            }
+        }
+
+        while (i < mid){
+            mix[k++] = nums[i++];
+        }
+
+        while (j < end){
+            mix[k++] = nums[j++];
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            nums[start + l] = mix[l];
+        }
     }
 
     private static int[] merge(int[] first, int[] second) {
